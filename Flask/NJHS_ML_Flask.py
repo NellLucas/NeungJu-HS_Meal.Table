e4819f6a22bb85d771fbbf1c9a30dac010f6a675
IrisEsther <seojaehyeong06@gmail.com>
2022-12-26 10:44:51 +0900

    feat: code update

--- Flask/NJHS_ML_Flask.py
+++ Flask/NJHS_ML_Flask.py
@@ -12,7 +12,7 @@ def student():
 def result():
    if request.method == 'POST':
     result = dict(request.form)
-    Ntime=result['date']
+    Ntime = result['date']
     day = Ntime[:4] + Ntime[5:7] + Ntime[8:10]
     Mcode = result['time']
     req = requests.get("https://stu.jne.go.kr/sts_sci_md01_001.do?schulCode=Q100000188&schulCrseScCode=4&schulKndScCode=04&"+"schMmealScCode="+f"{Mcode}"+"&schYmd="+f"{day}")
@@ -22,11 +22,11 @@ def result():
         week = 0
     if(Mcode=='1'or Mcode=='2' or Mcode=='3'):
         if(Mcode=='1'):
-            NJtime='아침'
+            NJtime = '아침'
         elif(Mcode=='2'):
-            NJtime='점심'
+            NJtime = '점심'
         else:
-            NJtime='저녁'
+            NJtime = '저녁'
         soup = BeautifulSoup(req.text, "html.parser")
         datas = soup.find_all("tr")
         datas = datas[2].find_all('td')
@@ -36,8 +36,8 @@ def result():
         for i in ['<br>','<td class="textC">','</td>','<td class="textC last">','.','&amp;']:
             datas = datas.replace(f'{i}','')
     else:
-        datas='잘못된 입력입니다'
-        NJtime=''
+        datas = '잘못된 입력입니다'
+        NJtime = ''
 
     return render_template("result.html",result=datas,date=Ntime,NJtime=NJtime)
 
